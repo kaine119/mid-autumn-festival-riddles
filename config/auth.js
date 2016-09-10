95022db1e852d83aa5388a22ae40e1da50b94691
Mui Kai En <muikaien1@gmail.com>
2016-09-10 11:55:49 +0800

    Add "play again" button after 5th question

--- config/auth.js
+++ config/auth.js
@@ -1,4 +1,4 @@
-var domain = process.env.DOMAIN || "localhost:3000"
+var domain = process.env.DOMAIN || "http://localhost:3000"
 
 module.exports = {
 	clientID: "888665451730-237678ts3vp6g648hjpr1ml9io7oaucl.apps.googleusercontent.com",
